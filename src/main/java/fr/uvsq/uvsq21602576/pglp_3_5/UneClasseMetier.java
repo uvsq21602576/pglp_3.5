69da0ed00a0d463ea4c6767538048de5322d88d1
Flora Carré <flora.carre@ens.uvsq.fr>
2020-03-04 11:41:47 +0100

    Ajout lasse metier. fait appel à logger

---
+++ src/main/java/fr/uvsq/uvsq21602576/pglp_3_5/UneClasseMetier.java
@@ -0,0 +1,19 @@
+package fr.uvsq.uvsq21602576.pglp_3_5;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class UneClasseMetier {
+	final static Logger logger = LoggerFactory.getLogger(UneClasseMetier.class);
+
+	public UneClasseMetier() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void uneMethodeMetier () {
+		logger.info("Debut_uneMethodeMetier");
+		//traitement
+		logger.info("Fin_uneMethodeMetier");
+	}
+
+}
